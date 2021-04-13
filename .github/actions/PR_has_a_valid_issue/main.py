d22db435309347890e1301c4d3bdf0407aad848b
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-13 04:42:50 +0200

    Update main.py

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -1,7 +1,7 @@
 import os
 
-print("This is the PR body", os.getenv("prbody"))
+print("This is the PR body", os.getenv("INPUT_PRBODY"))
 print("*************************")
-print(os.getenv("prbody"))
+print(os.getenv("INPUT_PRBODY"))
 print("*************************")
-print(type(os.getenv("prbody")))
+print(type(os.getenv("INPUT_PRBODY")))
