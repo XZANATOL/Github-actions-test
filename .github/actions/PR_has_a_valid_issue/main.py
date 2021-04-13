cb24d609137a07394ef5c94b25241ff2102c3027
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-13 19:29:46 +0200

    check complete URL

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -9,7 +9,9 @@ issue_num = re.search(pattern, body)[0].replace("#", "")
 
 # url list will be something like this
 # ['https:', '', 'api.github.com', 'repos', 'owner', 'repo-name']
-url = url.split("/")[:-2]  # Split URL using slashes
-url[2] = url[2].replace("api", "www")  # Replace api with html url
-url.pop(3)  # git rid of "repos" record
+url = url.split("/")[:-2]               # Split URL using slashes
+url[2] = url[2].replace("api", "www")   # Replace API URL with HTML URL
+url.pop(3)                              # Get rid of "repos" record
+url = "/".join(url)                     # Reattach URL pieces
+url += "/issues/{}".format(issue_num)   # Add issue number
 print(url)
