bca23d8dffc17db5be6be8789657b9e615c149d8
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-13 19:26:27 +0200

    get correct url before adding issue num

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -5,6 +5,11 @@ pattern = "#\d+"  # Used pattern to search for Issues.
 body = os.getenv("INPUT_PRBODY")  # PR body
 url  = os.getenv("INPUT_PRURL")   # PR URL
 
-issue = re.search(pattern, body)[0].replace("#", "")
-url = "/".join( url.split("/")[:-2] )
+issue_num = re.search(pattern, body)[0].replace("#", "")
+
+# url list will be something like this
+# ['https:', '', 'api.github.com', 'repos', 'owner', 'repo-name']
+url = url.split("/")[:-2]  # Split URL using slashes
+url[2] = url[2].replace("api", "www")  # Replace api with html url
+url.pop(3)  # git rid of "repos" record
 print(url)
