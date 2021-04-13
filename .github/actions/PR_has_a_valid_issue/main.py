176acb6f46bf27413da5445db9510ecd1b1c8e49
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-13 04:19:31 +0200

    more checks on type of pr_body var

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -1,3 +1,7 @@
 import os
 
-print("This is the PR body" + os.getenv("pr_body"))
+print("This is the PR body", os.getenv("pr_body"))
+print("*************************")
+print(os.getenv("pr_body"))
+print("*************************")
+print(type(os.getenv("pr_body")))
