6a8f706a445d5899ddf395b993fc0660705c024c
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-13 19:08:24 +0200

    First validation edit

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -1,9 +1,10 @@
 import os, re
 
-pattern = "#\d+"
+pattern = "#\d+"  # Used pattern to search for Issues.
 
-body = os.getenv("INPUT_PRBODY")
-print("*************************")
-print(body)
-print("*************************")
-print(re.search(pattern, body))
+body = os.getenv("INPUT_PRBODY")  # PR body
+url  = os.getenv("INPUT_PRURL")   # PR URL
+
+issue = re.search(pattern, body)[0].replace("#", "")
+print(issue)
+print(url)
