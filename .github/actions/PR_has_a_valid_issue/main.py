950f07fa2c6436b22d4b3b2ede2d2f488d41911d
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-14 02:04:38 +0200

    validation process test 1

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -1,5 +1,5 @@
-import os, re
-
+import os, re, requests
+"""
 pattern = "#\d+"  # Used pattern to search for Issues.
 
 body = os.getenv("INPUT_PRBODY")  # PR body
@@ -14,4 +14,18 @@ url[2] = url[2].replace("api", "www")   # Replace API URL with HTML URL
 url.pop(3)                              # Get rid of "repos" record
 url = "/".join(url)                     # Reattach URL pieces
 url += "/issues/{}".format(issue_num)   # Add issue number
-print(url)
+"""
+url = 
+valid = 0                               # Is valid code
+response = requests.get(url)
+if response.status_code == 200:         # Check if not a 404 page
+    if response.url == url:             # Check if not redirected to a pull request page
+        # Check if Issue is open not closed
+        text = response.text
+        pattern_issue = "Status:\s(\w+)"
+        if re.search(pattern_issue, text)[1] == "Open":
+            valid = 1
+
+print("Valid flag is now", valid)
+        
+        
