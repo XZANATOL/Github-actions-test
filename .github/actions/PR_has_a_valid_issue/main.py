be9610a61d343d50802871d5dbc2dcdb0ec30b9c
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-14 14:01:41 +0200

    Update main.py

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -10,12 +10,12 @@ issue_num = re.search(pattern, body)[0].replace("#", "")
 # url list will be something like this
 # ['https:', '', 'api.github.com', 'repos', 'owner', 'repo-name']
 url = url.split("/")[:-2]               # Split URL using slashes
-url[2] = url[2].replace("api.", "")   # Replace API URL with HTML URL
+url[2] = url[2].replace("api.", "")     # Replace API URL with HTML URL
 url.pop(3)                              # Get rid of "repos" record
 url = "/".join(url)                     # Reattach URL pieces
 url += "/issues/{}".format(issue_num)   # Add issue number
 
-valid_code = False                      # Is valid code
+valid_code = 0                          # Is valid code
 response = requests.get(url)
 if response.status_code == 200:         # Check if not a 404 page
     print("status code is 200")
@@ -25,12 +25,14 @@ if response.status_code == 200:         # Check if not a 404 page
         text = response.text
         pattern_issue = "Status:\s(\w+)"
         if re.search(pattern_issue, text)[1] == "Open":
-            valid_code = True
+            valid_code = 1
         else:
             print("Couldn't find Open flag")
+else:
+    print("Invalid Response Code obtained - error code: {}".format(response.status_code))
 
 print("Valid flag is:", valid_code)
 
-f"::set-output name=valid::{valid_code}"
+print(f"::set-output name=valid::{valid_code}")
         
         
