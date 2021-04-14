9fdae7118031020848a792f9fc6efd369447db49
XZANATOL <abdelrahmanbedox@gmail.com>
2021-04-14 02:14:32 +0200

    validation process test 1 resolved error

--- .github/actions/PR_has_a_valid_issue/main.py
+++ .github/actions/PR_has_a_valid_issue/main.py
@@ -10,7 +10,7 @@ issue_num = re.search(pattern, body)[0].replace("#", "")
 # url list will be something like this
 # ['https:', '', 'api.github.com', 'repos', 'owner', 'repo-name']
 url = url.split("/")[:-2]               # Split URL using slashes
-url[2] = url[2].replace("api", "www")   # Replace API URL with HTML URL
+url[2] = url[2].replace("api.", "")   # Replace API URL with HTML URL
 url.pop(3)                              # Get rid of "repos" record
 url = "/".join(url)                     # Reattach URL pieces
 url += "/issues/{}".format(issue_num)   # Add issue number
